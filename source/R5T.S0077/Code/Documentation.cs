b4fafa2f361e30933489696956a5627c367e08ee
davidcoats <david@getsafetycone.com>
2023-06-10 09:16:12 -0700

    Initial commit.

---
+++ source/R5T.S0077/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0077
+{
+	/// <summary>
+	/// MSBuild locator scripts.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.S0077/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.S0077
+{
+    public static class Instances
+    {
+		
+    }
+}
